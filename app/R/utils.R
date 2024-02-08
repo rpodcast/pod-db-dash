758687e49a9e413329151c6a78194ed0a8934709
Eric Nantz <thercast@gmail.com>
2024-02-08 03:52:07 +0000

    add functions for downloading the pointblank and podcast dup data objects

--- app/R/utils.R
+++ app/R/utils.R
@@ -1,3 +1,33 @@
 hello_world <- function() {
   print("Hello World!")
 }
+
+podcastdb_pointblank_object <- function() {
+  tmp_file <- tempfile(pattern = "pointblank")
+  download.file(
+    url = config::get("pointblank_object_path"),
+    destfile = tmp_file
+  )
+
+  res <- pointblank::x_read_disk(
+    filename = fs::path_file(tmp_file),
+    path = fs::path_dir(tmp_file)
+  )
+
+  unlink(tmp_file)
+  return(res)
+}
+
+podcastdb_dupdf_object <- function() {
+  tmp_file <- tempfile(pattern = "pointblank")
+
+  download.file(
+    url = config::get("podcast_dup_df_path"),
+    destfile = tmp_file
+  )
+
+  res <- readRDS(tmp_file)
+
+  unlink(tmp_file)
+  return(res)
+}
